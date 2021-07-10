4108ca73ca723446a15493f747b697ce91980183
Isekaï <65531779+DevSekai@users.noreply.github.com>
2021-07-10 15:26:09 +0200

    Update Blips.lua

--- Core/Client/Modules/Blips.lua
+++ Core/Client/Modules/Blips.lua
@@ -6,23 +6,39 @@ Blips.__index = Blips
     
     *Create blip*
     local [retVal] = Blips.create(Label, Pos, Sprite, Colour) -- Create Blip
-    *Set blip*
-    [retVal]:SetScale(int) -- Change Blip Scale
 ]]
 
-function Blips.create(Label, Pos, Sprite, Colour)
+function Blips.create(params)
     local self = {}
 
-    self.id = AddBlipForCoord(Pos)
-    SetBlipSprite(self.id, Sprite)
-    SetBlipColour(self.id, Colour)
-    BeginTextCommandSetBlipName('STRING') 
-    AddTextComponentSubstringPlayerName(Label) 
-    EndTextCommandSetBlipName(self.id)
+    self.id = nil
+    self.label = params.label or nil
+    self.pos = params.pos or nil
+    self.sprite = params.sprite or nil
+    self.colour = params.colour or nil
+    self.scale = params.scale or nil
+
 
     return setmetatable(self, Blips)
 end
 
-function Blips:SetScale(int)
-    SetBlipScale(self.id, int)
+function Blips:blipCoords()
+    self.id = AddBlipForCoord(self.pos)
+    SetBlipSprite(self.id, self.sprite)
+    SetBlipColour(self.id, self.colour)
+    SetBlipScale(self.id, self.scale)
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentSubstringPlayerName(self.label)
+    EndTextCommandSetBlipName(self.id)
+    return self.id
+end
+
+function Blips:entityBlips(entityId)
+    self.id = AddBlipForEntity(entityId)
+    SetBlipSprite(self.id, self.sprite)
+    SetBlipColour(self.id, self.colour)
+    SetBlipScale(self.id, self.scale)
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentSubstringPlayerName(self.label)
+    EndTextCommandSetBlipName(self.id)
 end
