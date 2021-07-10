79f09f14b3ab118065d520dc73348e77136322a3
ZAKIO <66121290+ZAKIO59@users.noreply.github.com>
2021-07-10 18:23:26 +0200

    Update Entity.lua

--- Core/Client/Modules/Entity.lua
+++ Core/Client/Modules/Entity.lua
@@ -7,35 +7,30 @@ function Entity.create(Hash, Pos, Sync)
     
     Stream:loadModel(Hash)
     self.id = CreateObject(GetHashKey(Hash), Pos.x, Pos.y, Pos.z, Sync, false, true)
-    self.pos = {x = Pos.x, y = Pos.y, z = Pos.z}
     self.exist = DoesEntityExist(self.id)
 
     return self
 end
 
 function Entity:setFreeze(bool)
-    Wait(50)
     if self.exist then
         FreezeEntityPosition(self.id, bool)
     end
 end
 
 function Entity:setCollision(bool)
-    Wait(50)
     if self.exist then
         SetEntityCollision(self.id, bool, bool)
     end
 end
 
 function Entity:setAlpha(int)
-    Wait(50)
     if self.exist then
         SetEntityAlpha(self.id, int)
     end
 end
 
 function Entity:delete()
-    Wait(50)
     if self.exist then
         DeleteObject(self.id)
     end
