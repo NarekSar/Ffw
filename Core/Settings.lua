47c3b0e94d10eaa96d65902597573f334a637967
ZAKIO <66121290+ZAKIO59@users.noreply.github.com>
2021-07-12 11:08:53 +0200

    Update Settings.lua
    Add pvp and wanted lvl setting

--- Core/Settings.lua
+++ Core/Settings.lua
@@ -1,6 +1,8 @@
 CoreSett = {
     useOneSync = true, -- For more feature if you use OneSync
     useMph = false, -- If false you will use Kmh
+    useWantedLvl = false, -- If false Wanted level are disable
+    usePvp = true, -- If true pvp are enable
     baseAccount = {money = 2500, bank = 7500, black = 0},
 }
 MenuSett = {
