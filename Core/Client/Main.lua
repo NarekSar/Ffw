26794aa04e03a2ca8e403d04bc96e620d45adf66
Isekaï <65531779+DevSekai@users.noreply.github.com>
2021-07-12 12:54:17 +0200

    Update Main.lua

--- Core/Client/Main.lua
+++ Core/Client/Main.lua
@@ -7,5 +7,15 @@ end)
 
 RegisterNetEvent("Ffw:InitPlayer")
 AddEventHandler("Ffw:InitPlayer", function()
+    for _,v in pairs (ZonesAdded) do
+        v:showZone()
+    end
     myPlayer = Player.new()
+    if not CoreSett.useWantedLvl then
+	    SetMaxWantedLevel(0)
+    end
+    if CoreSett.usePvp then
+        SetCanAttackFriendly(PlayerPedId(), true, false)
+	    NetworkSetFriendlyFireOption(true)
+    end
 end)
