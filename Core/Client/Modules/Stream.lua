81d006581ec4ebc06f0ffe97492c5dbdddb3c593
Isekaï <65531779+DevSekai@users.noreply.github.com>
2021-07-14 18:32:40 +0200

    Update Stream.lua

--- Core/Client/Modules/Stream.lua
+++ Core/Client/Modules/Stream.lua
@@ -1,19 +1,15 @@
 Stream = {}
 
 function Stream:loadAnimDict(dict)
-    Citizen.CreateThread(function()
-        RequestAnimDict(dict)
-        while not HasAnimDictLoaded(dict) do
-            Wait(50)
-        end
-    end)
+    RequestAnimDict(dict)
+    while not HasAnimDictLoaded(dict) do
+        Wait(50)
+    end
 end
 
 function Stream:loadModel(model)
-    Citizen.CreateThread(function()
-        RequestModel(model)
-        while not HasModelLoaded(model) do
-            Wait(50)
-        end
-    end)
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(50)
+    end
 end
